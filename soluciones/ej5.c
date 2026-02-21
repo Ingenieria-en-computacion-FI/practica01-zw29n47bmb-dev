55fa2899fa19d058b72702927a31faec9bd67d40
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-02-20 21:43:03 -0600

    Implement solution for exercise 5 in ej5.c
    Add calculations and output for exercise 5

--- soluciones/ej5.c
+++ soluciones/ej5.c
@@ -2,5 +2,18 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 5
+     long base = 1000;
+    int size = sizeof(int);
+
+    long real = base + 10*size;
+    long afirmacion = 1010;
+
+    printf("Direccion real de a[10] = %ld\n", real);
+    printf("Afirmacion estudiante = %ld\n", afirmacion);
+
+    if(real == afirmacion)
+        printf("Correcto\n");
+    else
+        printf("Incorrecto\n");
     return 0;
 }

c4e8e0e47a6f9ac69ed4a224afb39533e27ab8fa
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-02-20 21:43:39 -0600

    Implement 2D array and print memory addresses
    Added a 2D array and print statements for its addresses.

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -2,5 +2,12 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 6
+     int m[2][4];
+
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 4; j++){
+            printf("&m[%d][%d] = %p\n", i, j, (void*)&m[i][j]);
+        }
+    }
     return 0;
 }

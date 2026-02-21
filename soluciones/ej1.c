5bd28268506f763c7ea4ba0d34c55dd3033f1983
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-02-20 21:26:58 -0600

    Implement memory address printing for array elements
    Added code to print the memory addresses of an array.

--- soluciones/ej1.c
+++ soluciones/ej1.c
@@ -2,5 +2,11 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 1
+    int main(){
+    int a[6];
+    
+    for (int i = 0; i < 6; i++) {
+        printf("%p\n", &a[i]);
+    }
     return 0;
 }

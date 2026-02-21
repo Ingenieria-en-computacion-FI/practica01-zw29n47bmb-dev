f29d73d79b7926de4c0701d79f3beb3f0529f32b
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-02-20 21:29:27 -0600

    Add integer arrays and print their memory addresses
    Added arrays a, b, and c to store integers and print their addresses.

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -2,5 +2,20 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 3
+     int a[3];
+    int b[3];
+    int c[3];
+
+    printf("Direcciones de a:\n");
+    for(int i = 0; i < 3; i++)
+        printf("&a[%d] = %p\n", i, (void*)&a[i]);
+
+    printf("\nDirecciones de b:\n");
+    for(int i = 0; i < 3; i++)
+        printf("&b[%d] = %p\n", i, (void*)&b[i]);
+
+    printf("\nDirecciones de c:\n");
+    for(int i = 0; i < 3; i++)
+        printf("&c[%d] = %p\n", i, (void*)&c[i]);
     return 0;
 }

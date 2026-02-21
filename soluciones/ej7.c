20ae8258f33152df4f98be3c9b8f8b3aaae8a04b
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-02-20 21:44:26 -0600

    Implement exercise 7 with memory address comparison
    Added a 2D array and comparison of memory addresses.

--- soluciones/ej7.c
+++ soluciones/ej7.c
@@ -2,5 +2,14 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 7
+    int m[2][4];
+
+    printf("&m[0][3] = %p\n",(void*)&m[0][3]);
+    printf("&m[1][0] = %p\n",(void*)&m[1][0]);
+
+    if(&m[1][0] > &m[0][3])
+        printf("&m[1][0] es mayor\n");
+    else
+        printf("&m[0][3] es mayor\n");
     return 0;
 }
